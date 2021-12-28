ca84abb598cca5472bac2819f2f7d99c00fbfdb5
whats2000 <60466660+whats2000@users.noreply.github.com>
2021-12-28 17:30:57 +0800

    Update LeNet_Control.h

--- Group_21_Final_Project/code/LeNet_Control.h
+++ Group_21_Final_Project/code/LeNet_Control.h
@@ -31,31 +31,19 @@ SC_MODULE(Control) {
  sc_out < DATA_TYPE > conv_0_data_in;
  sc_out < int > conv_0_data_size;
 
- // connect to Conv Submodule 1
- sc_in < DATA_TYPE > conv_1_data_out;
- sc_out < DATA_TYPE > conv_1_data_in;
- sc_out < int > conv_1_data_size;
-
  // connect to Pooling Submodule 0
  sc_in < DATA_TYPE > pool_0_data_out;
  sc_out < DATA_TYPE > pool_0_data_in;
  sc_out < int > pool_0_data_size;
 
- // connect to Pooling Submodule 1
- sc_in < DATA_TYPE > pool_1_data_out;
- sc_out < DATA_TYPE > pool_1_data_in;
- sc_out < int > pool_1_data_size;
-
- // connect to Dense Submodule
+ // connect to Dense Submodule 0
  sc_in < DATA_TYPE > dens_data_out;
  sc_out < DATA_TYPE > dens_data_in;
  sc_out < int > dens_data_size;
 
  Conv* Conv_0;
- Conv* Conv_1;
 
  Pool* Pool_0;
- Pool* Pool_1;
 
  Dens* Dens_0;
 
@@ -66,14 +54,8 @@ SC_MODULE(Control) {
   Conv_0 = new Conv("Conv_0");
   Conv_0->clk(clk);
 
-  Conv_1 = new Conv("Conv_1");
-  Conv_1->clk(clk);
-
   Pool_0 = new Pool("Pool_0");
   Pool_0->clk(clk);
-
-  Pool_1 = new Pool("Pool_1");
-  Pool_1->clk(clk);
 
   Dens_0 = new Dens("Dens_0");
   Dens_0->clk(clk);
