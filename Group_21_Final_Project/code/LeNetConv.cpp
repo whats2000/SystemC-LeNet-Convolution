ca89c2d902da9a34122557de75e2afc84467785b
Yang021024 <abcd112724@gmail.com>
2021-12-31 05:21:23 +0800

    use data_size in conv.cpp

--- Group_21_Final_Project/code/LeNetConv.cpp
+++ Group_21_Final_Project/code/LeNetConv.cpp
@@ -12,13 +12,13 @@ void Conv::conv_run() {
   for (int i = 0; i < 25; i++)
    kernel[i] = 0;
 
-  for (int i = 0; i < 140; i++)
+  for (int i = 0; i < data_size * 5; i++)
    data[i] = 0;
  }
  else {
   cout << ">> Conv clock = " << clock_cycle << endl;
 
-  if (clock_cycle >= 1 && clock_cycle <= 810) {
+  if (clock_cycle >= 1 && clock_cycle <= 26) {
    //cout << ">> Conv data in = " << data_in.read() << endl;
 
    if (clock_cycle < 26) {
@@ -32,14 +32,14 @@ void Conv::conv_run() {
     //cout << "bias = " << bias << endl;
    }
   }
-  else if (clock_cycle >= 27 && clock_cycle <= 810) {
-   data[(clock_cycle - 27) % 140] = data_in.read();
+  else if (clock_cycle >= 27 && clock_cycle <= data_size * data_size + 26) {
+   data[(clock_cycle - 27) % data_size * 5 ] = data_in.read();
 
-   if (clock_cycle >= 143 && clock_cycle <= 810) {
-    if ((clock_cycle - 143) % 28 <= 23) {
+   if (clock_cycle >= (31 + data_size * 4) && clock_cycle <= data_size * data_size + 26 ) {
+    if ((clock_cycle - (31 + data_size * 4)) % data_size <= data_size - 5) {
      for (int j = 0; j < 5; j++)
       for (int i = 0; i < 5; i++)
-       temp_sum += data[(clock_cycle - 143 + i + 28 * j) % 140] * kernel[i + 5 * j];
+       temp_sum += data[(clock_cycle - (31 + data_size * 4) + i + data_size * j) % data_size * 5] * kernel[i + 5 * j];
 
      temp_sum += bias;
 
