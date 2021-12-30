514e30a92bf827c12938db61cde088995e829f91
whats2000 <60466660+whats2000@users.noreply.github.com>
2021-12-30 13:43:28 +0800

    Update LeNetControl.cpp reset

--- Group_21_Final_Project/code/LeNetControl.cpp
+++ Group_21_Final_Project/code/LeNetControl.cpp
@@ -2,11 +2,34 @@
 
 void Control::control_run() {
  if (rst.read()) {
+  rom_rd.write(true);
+  rom_addr.write(0);
+
+  ram_wr.write(false);
+  ram_addr.write(0);
+  ram_data_in.write(0);
+
+  result.write(0);
+  output_valid.write(false);
+
+  // connect to Conv Submodule 0
+  conv_0_data_in.write(0);
+  conv_0_data_size.write(28);
   conv_0_rst.write(true);
+
+  // connect to Pooling Submodule 0
+  pool_0_data_in.write(0);
+  pool_0_data_size.write(24);
   pool_0_rst.write(true);
+
+  // connect to Dense Submodule 0
+  dens_0_data_in.write(0);
+  dens_0_data_size.write(256);
   dens_0_rst.write(true);
  }
  else {
+  
 
+  clock_cycle++;
  }
 }
